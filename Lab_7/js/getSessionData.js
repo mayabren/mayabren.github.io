2176a860d6fab28f0530530a7c80ff6a4f6e78c1
Maya Brenner-Letich <mayabren@terpmail.umd.edu>
2020-11-06 14:48:29 -0500

    final

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,9 @@
 // Get saved data from sessionStorage
-let title = route.title; // Your code here
-let lat_max = route.lat_max ;
-let lat_min = route.lat_min;
-let lon_max = route.lon_max;
-let lon_min = route.lon_min;
+let title = sessionStorage.getItem('title'); // Your code here
+let lat_max = sessionStorage.getItem('lat_max');
+let lat_min = sessionStorage.getItem('lat_min');
+let lon_max = sessionStorage.getItem('lon_max');
+let lon_min = sessionStorage.getItem('lon_min');
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -13,6 +13,11 @@ function getBusRoute() {
       })
       .then((route) => {
         console.log(route);
+        sessionStorage.setItem('title', route.title);
+        sessionStorage.setItem('lat_max', route.lat_max);
+        sessionStorage.setItem('lat_min', route.lat_min);
+        sessionStorage.setItem('lon_max', route.lon_max);
+        sessionStorage.setItem('lon_min', route.lon_min);
       })
       .catch((err) => {
         console.log(err);
